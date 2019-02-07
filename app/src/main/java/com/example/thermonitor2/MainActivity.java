98613670a6c695e769995d69042bc0740e211839
fatmaabdelmageed <Ff@351998>
2019-02-07 17:08:00 +0200

    thermonitor

--- app/src/main/java/com/example/thermonitor2/MainActivity.java
+++ app/src/main/java/com/example/thermonitor2/MainActivity.java
@@ -7,15 +7,42 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
-    private EditText email,password;
-    private Button btnLogin;
+     EditText email,password;
+     Button btnLogin;
+     Button register;
+    TextView signup;
+    ImageView lock;
+    FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        firebaseAuth = FirebaseAuth.getInstance();
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
+        lock= findViewById(R.id.lock);
+        signup = findViewById(R.id.registertext);
+        register = findViewById(R.id.registerbutton);
         btnLogin = (Button) findViewById(R.id.button);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,8 +52,61 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Toast.makeText(MainActivity.this, "User logged in ", Toast.LENGTH_SHORT).show();
+                    Intent I = new Intent(MainActivity.this, ListActivity.class);
+                    startActivity(I);
+                } else {
+                    Toast.makeText(MainActivity.this, "Login to continue", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent I = new Intent(MainActivity.this, ListActivity.class);
+                startActivity(I);
+            }
+        });
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userEmail = email.getText().toString();
+                String userPaswd = password.getText().toString();
+                if (userEmail.isEmpty()) {
+                    email.setError("Provide your Email first!");
+                    email.requestFocus();
+                } else if (userPaswd.isEmpty()) {
+                    password.setError("Enter Password!");
+                    password.requestFocus();
+                } else if (userEmail.isEmpty() && userPaswd.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Fields Empty!", Toast.LENGTH_SHORT).show();
+                } else if (!(userEmail.isEmpty() && userPaswd.isEmpty())) {
+                    firebaseAuth.signInWithEmailAndPassword(userEmail, userPaswd).addOnCompleteListener(MainActivity.this, new OnCompleteListener() {
+                        @Override
+                        public void onComplete(@NonNull Task task) {
+                            if (!task.isSuccessful()) {
+                                Toast.makeText(MainActivity.this, "Not sucessfull", Toast.LENGTH_SHORT).show();
+                            } else {
+                                startActivity(new Intent(MainActivity.this, ListActivity.class));
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
     }
 }
+
+
+
+
+
 
---
+++ app/src/main/java/com/example/thermonitor2/RegisterActivity.java
@@ -0,0 +1,82 @@
+package com.example.thermonitor2;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.net.PasswordAuthentication;
+
+public class RegisterActivity<email, password> extends AppCompatActivity {
+
+        public EditText emailId, passwd;
+        public Button btnSignUp; //register button
+        public Button login;
+        public TextView signIn, signUp; //login
+        FirebaseAuth firebaseAuth;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_register);
+            firebaseAuth = FirebaseAuth.getInstance();
+            emailId = findViewById(R.id.username);
+            passwd = findViewById(R.id.password);
+            btnSignUp = findViewById(R.id.register);
+            signUp = findViewById(R.id.signup);
+            login= findViewById(R.id.login);
+            signIn = findViewById(R.id.signin);
+            btnSignUp.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String emailID = emailId.getText().toString();
+                    String paswd = passwd.getText().toString();
+                    if (emailID.isEmpty()) {
+                        emailId.setError("Provide your Email first!");
+                        emailId.requestFocus();
+                    } else if (paswd.isEmpty()) {
+                        passwd.setError("Set your password");
+                        passwd.requestFocus();
+                    } else if (emailID.isEmpty() && paswd.isEmpty()) {
+                        Toast.makeText(RegisterActivity.this, "Fields Empty!", Toast.LENGTH_SHORT).show();
+                    } else if (!(emailID.isEmpty() && paswd.isEmpty())) {
+                        firebaseAuth.createUserWithEmailAndPassword(emailID, paswd).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener() {
+                            @Override
+                            public void onComplete(@NonNull Task task) {
+
+                                if (!task.isSuccessful()) {
+                                    Toast.makeText(RegisterActivity.this.getApplicationContext(),
+                                            "SignUp unsuccessful: " + task.getException().getMessage(),
+                                            Toast.LENGTH_SHORT).show();
+                                } else {
+                                    startActivity(new Intent(RegisterActivity.this,     ListActivity.class));
+                                }
+                            }
+                        });
+                    } else {
+                        Toast.makeText(RegisterActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+            signIn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent I = new Intent(RegisterActivity.this, MainActivity.class);
+                    startActivity(I);
+                }
+            });
+        }
+    }
